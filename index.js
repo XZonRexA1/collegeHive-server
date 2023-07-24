064b541fc3638d048a057f7121c56008bafeed55
Alif Chowdhury <chowdhuryalif42@gmail.com>
2023-07-24 22:00:07 +0600

    other stuff added

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-require('dotenv').config()
-const port  = process.env.PORT || 5000;
-
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1fwa7gk.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -19,35 +17,92 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-     client.connect();
+    client.connect();
 
     // college collection
-    const collegeCollection = client.db('collegeDB').collection('college')
-    const addCollegeCollection = client.db('collegeDB').collection('addCollege')
+    const collegeCollection = client.db("collegeDB").collection("college");
+    const addCollegeCollection = client
+      .db("collegeDB")
+      .collection("addCollege");
 
+    // review collection
+    const addReviewCollection = client.db("collegeDB").collection("addReview");
+    // userinfo collection
+    const userInfoCollection = client.db("collegeDB").collection("userInfo");
 
     // college related api
     app.get("/college", async (req, res) => {
-        const result = await collegeCollection.find().toArray();
-        res.send(result);
-      });
+      const result = await collegeCollection.find().toArray();
+      res.send(result);
+    });
 
     // add college details info
-    app.post('/addCollege', async(req,res)=>{
+    app.get("/addCollege", async (req, res) => {
+      const result = await addCollegeCollection.find().toArray();
+      res.send(result);
+    });
+    app.post("/addCollege", async (req, res) => {
       const addCollege = req.body;
       const result = await addCollegeCollection.insertOne(addCollege);
       res.send(result);
-    })
+    });
+
+    // review related apis
+    app.get("/addReview", async (req, res) => {
+      const result = await addReviewCollection.find().toArray();
+      res.send(result);
+    });
+    app.post("/addReview/:id", async (req, res) => {
+      const addReview = req.body;
+      const result = await addReviewCollection.insertOne(addReview);
+      res.send(result);
+    });
+
+    // user related apis
+
+    // get user info
+    app.get("/userInfo", async (req, res) => {
+      const result = await userInfoCollection.find().toArray();
+      res.send(result);
+    });
+
+    // post user info
+    app.post("/userInfo", async (req, res) => {
+      const userInfo = req.body;
+      const result = await userInfoCollection.insertOne(userInfo);
+      res.send(result);
+    });
+
+
+    app.put("/userInfo", async (req, res) => {
+      const updatedData = req.body;
+      const filter = { _id: updatedData._id }; 
+      delete updatedData._id; 
+      const updateDoc = {
+        $set: updatedData,
+      };
+      try {
+        const result = await userInfoCollection.updateOne(filter, updateDoc);
+        res.send(result);
+      } catch (error) {
+        console.error("Error updating user info:", error);
+        res.status(500).send("Error updating user info.");
+      }
+    });
+
+    
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -55,11 +110,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("college Hive is running");
+});
 
-app.get('/', (req,res)=>{
-    res.send('college Hive is running')
-})
-
-app.listen(port, ()=>{
-    console.log(`College Hive is running on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`College Hive is running on port ${port}`);
+});
