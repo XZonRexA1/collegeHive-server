84446eea9277ed5d3f6a7902e06930707ab3db64
Alif Chowdhury <chowdhuryalif42@gmail.com>
2023-07-24 12:27:34 +0600

    add college api added

--- index.js
+++ index.js
@@ -9,6 +9,53 @@ const port  = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1fwa7gk.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+     client.connect();
+
+    // college collection
+    const collegeCollection = client.db('collegeDB').collection('college')
+    const addCollegeCollection = client.db('collegeDB').collection('addCollege')
+
+
+    // college related api
+    app.get("/college", async (req, res) => {
+        const result = await collegeCollection.find().toArray();
+        res.send(result);
+      });
+
+    // add college details info
+    app.post('/addCollege', async(req,res)=>{
+      const addCollege = req.body;
+      const result = await addCollegeCollection.insertOne(addCollege);
+      res.send(result);
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 app.get('/', (req,res)=>{
     res.send('college Hive is running')
 })
